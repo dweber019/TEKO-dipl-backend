2a6eed54cb7414b0f884a39f499b20b8562ce827
David Weber <david.weber@w3tec.ch>
2017-09-20 20:04:01 +0200

    Implement model for users

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'firstname', 'lastname', 'auth0_id', 'invite_token',
     ];
 
     /**
@@ -24,6 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'invite_token', 'auth0_id',
     ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('auth0_id');
+            $table->string('invite_token');
+//            $table->string('email')->unique();
+//            $table->string('password');
+//            $table->rememberToken();
             $table->timestamps();
         });
     }
