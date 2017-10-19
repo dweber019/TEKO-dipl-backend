87531ba893ed0fcd9ed7147ab6e94d4c4b8b915d
David Weber <david.weber@w3tec.ch>
2017-10-19 16:15:24 +0200

    Test email notification on user creation

--- tests/Feature/UserInvitationTest.php
+++ tests/Feature/UserInvitationTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Mail\UserInvitation;
 use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class UserInvitationTest extends TestCase
@@ -27,7 +29,7 @@ class UserInvitationTest extends TestCase
 
         $response = $this->actingAs($actingUser)->postJson('/api/users', $this->userPost);
 
-        $response->assertStatus(201);
+        $response->assertSuccessful();
     }
 
     public function test_wrong_input_data()
@@ -46,6 +48,29 @@ class UserInvitationTest extends TestCase
         $response = $this->actingAs($actingUser)->postJson('/api/users', $this->userPost);
 
         $response->assertStatus(403);
+    }
+
+    public function test_mail_on_create_user()
+    {
+        $actingUser = User::find(1);
+
+        Mail::fake();
+
+        $response = $this->actingAs($actingUser)->postJson('/api/users', $this->userPost);
+        $response->assertSuccessful();
+
+        Mail::assertSent(UserInvitation::class, 1);
+
+        $userInvite = $this->userPost;
+
+        Mail::assertSent(UserInvitation::class, function ($mail) use ($userInvite) {
+            return $mail->hasTo($userInvite['inviteEmail']);
+        });
+
+        Mail::assertSent(UserInvitation::class, function ($mail) use ($userInvite) {
+            return $mail->invitation->firstname === $userInvite['firstname'] &&
+              $mail->invitation->lastname === $userInvite['lastname'];
+        });
     }
 
 }
